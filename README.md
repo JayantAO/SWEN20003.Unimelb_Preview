ae7b1814fc653bce95cbd21352862e55e1a54e22
JayantAo <jayant.ao9787@gmail.com>
2026-06-29 17:04:04 +0800

    feat:数组相关练习,主要是常规的遍历方法,接触二维数组

--- README.md
+++ README.md
@@ -11,14 +11,23 @@
 - nonprimitive type引用数据类型（也叫reference type）
 
 ### Constant 常量：
--全大写（正常变量名是驼峰）, eg final int MAX_LENGTH = 400
+- 全大写（正常变量名是驼峰）, eg final int MAX_LENGTH = 400
 
 ### Arithmetic & Relational Operators & Logic Operators 数学运算符与操作运算符与逻辑运算符：
--与C基本一致
--python 的逻辑运算用的是 AND OR NOT, 而 C 和 JAVA 则是&& || ！
+- 与C基本一致
+- python 的逻辑运算用的是 AND OR NOT, 而 C 和 JAVA 则是&& || ！
 
 ### Increment Operators累加、Decrement Operators累减、Conditional Operators条件运算符
--x = (a > b)?:等效于if a>b 则 这一类三元运算符
+- x = (a > b)?:等效于if a>b 则 这一类三元运算符
+
+## 循环
+- 两种 for 写法，包括  for(初始化;循环条件;迭代){}、  for(变量或者叫循环元素：循环容器如数组){}
+例如for(int item:num){}  （这里的item是数组容器num内部存储的每一个元素）；for(char c:s.toCharArray())  对于字符串，在使用这个方式做循环之前要用这个方法把字符串转化成字符数组
+
+但是for的增强写法本质是一种语法糖，不用太过在意
+
+
+## 数组
 
 ## 进度：
 学习到getters and setters
--- src/week03/Main.java
+++ src/week03/Main.java
@@ -1,4 +1,7 @@
 package week03;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -55,5 +58,126 @@ public class Main {
                 break;
         }
 
+//        数组定义练习
+        double[] num = new double[10];
+        for(int i = 0; i < 10; i++){
+            num[i] = Math.random();
+            System.out.println(num[i]);
+        }
+//        随机数函数
+        Random random = new Random();
+        int numInt = random.nextInt(10);
+        System.out.println(numInt);
+
+//        数组循环增强
+        for(double i:num){
+//            这里的i是num[i]而不是index
+//            也可以是for(double item:num){}
+            System.out.println(num);
+            System.out.println(i);
+        }
+        System.out.println(num[1]);
+
+//        找最大元素
+        int[] arrInt = new int[10];
+//        遍历初始化数组
+        for(int i = 0; i < arrInt.length; i++){
+//            数组.length    java有类函数，所以有这种类似python的方法
+            arrInt[i] = random.nextInt(100);
+        }
+//        遍历找最大值
+//        int result = 0;
+//        for(int i = 0; i < (arrInt.length-1); i++){
+//            if(arrInt[i]<arrInt[i+1]){
+//                result = arrInt[i+1];
+//            }
+//        }这里逻辑有问题
+        int result = arrInt[0];
+        for(int i = 1; i < arrInt.length; i++){
+            if(result<arrInt[i]){
+                result = arrInt[i];
+            }
+        }
+        System.out.println(result);
+
+
+//        找最大值优化
+        int[] arrIntV2 = new int[10];
+        int resultV2 = arrIntV2[0];
+        //        当数组内部没有放入值的时候，里面的元素是0
+        int indexV2 = 0;
+        System.out.println(resultV2);
+//        遍历初始化数组
+        for(int i = 0; i < arrIntV2.length; i++){
+            arrIntV2[i] = random.nextInt(100);
+            if(resultV2<arrIntV2[i]){
+                resultV2 = arrIntV2[i];
+                indexV2 = i;
+            }
+        }
+        System.out.println(resultV2);
+        System.out.println(indexV2);
+
+//        统计数组中超过平均值的元素个数
+        int[] arrIntV3 = new int[20];
+        int sumV3 = 0;
+        int avgV3 = 0;
+//        遍历初始化数组并求和求平均
+        for(int i = 0; i < arrIntV3.length; i++){
+            arrIntV3[i] = random.nextInt(100);
+            sumV3 = sumV3 + arrIntV3[i];
+        }
+        avgV3 = sumV3/arrIntV3.length;
+//        遍历找超过平均值的元素
+        int jV3 = 0;
+        for(int item:arrIntV3){
+            if(item > avgV3){
+                jV3 = jV3 + 1;
+            }
+        }
+        System.out.println(jV3);
+
+
+//        分别找出数组中的奇偶数(并各自存储输出)
+        int[] arrIntV4 = new int[50];
+        int iOdd = 0;
+        int iEve = 0;
+//        遍历初始化数组,统计奇偶数各自个数(目前还没有学习类似python的不需要知道index的容器)
+        for(int i = 0; i < arrIntV4.length; i++){
+            arrIntV4[i] = random.nextInt(200);
+            if(arrIntV4[i]%2 == 0){
+                iEve = iEve + 1;
+            }else{
+                iOdd = iOdd + 1;
+            }
+        }
+
+        int[] arrOdd = new int[iOdd];
+        int[] arrEve = new int[iEve];
+
+        int pointOdd = 0;
+        int pointEve = 0;
+
+        for(int item:arrIntV4){
+            if(item%2 != 0){
+                arrOdd[pointOdd] = item;
+                pointOdd++;
+            }else{
+                arrEve[pointEve] = item;
+                pointEve++;
+                }
+        }
+
+//        二维数组
+        int [][]arr2Division = new int[4][7];
+
+        int [][]arr2DivisionV2 = {{2,4,6,8,5},{0,0,1,1,4},{3,5,7,1,1},{1,1,1,2,4}};
+
+        for(int i = 0; i<arr2DivisionV2.length;i++){
+            for(int j = 0; j<arr2DivisionV2[i].length;j++){
+                System.out.print(arr2DivisionV2[i][j]);
+            }
+            System.out.println(" ");
+        }
     }
 }
